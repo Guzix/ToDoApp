336fd9191bd5e6da116ea8f662560a3192cad5ad
Guzix <marcinguzy.it@gmail.com>
2022-05-28 00:18:50 +0200

    test

---
+++ src/main/java/pl/guxiz/toDoApp/repository/BaseRepository.java
@@ -0,0 +1,8 @@
+package pl.guxiz.toDoApp.repository;
+
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class BaseRepository  {
+}
---
+++ src/main/java/pl/guxiz/toDoApp/service/BaseService.java
@@ -0,0 +1,8 @@
+package pl.guxiz.toDoApp.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BaseService {
+
+}
